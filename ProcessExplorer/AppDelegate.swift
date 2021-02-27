c965a1799ee569a805158b79ff214acf073557d7
Prashant Gautam <prashant.gtm1001@gmail.com>
2021-02-27 10:41:30 +0530

    Project folder added

---
+++ ProcessExplorer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ProcessExplorer
+//
+//  Created by Prashant Gautam on 27/02/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ProcessExplorer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ProcessExplorer
+//
+//  Created by Prashant Gautam on 27/02/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
