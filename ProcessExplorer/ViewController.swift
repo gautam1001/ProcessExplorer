8a2eda4eaa24ed93b4d3b41c40f4c86e170e1aa5
Prashant Gautam <prashant.gtm1001@gmail.com>
2021-02-27 10:58:11 +0530

    fetch running processes
    'fetchProcesses' function to fetch running processes

--- ProcessExplorer/ViewController.swift
+++ ProcessExplorer/ViewController.swift
@@ -13,11 +13,24 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.fetchProcesses()
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
+        }
+    }
+    
+    private func fetchProcesses(){
+        let workspace = NSWorkspace.shared
+        let applications = workspace.runningApplications
+
+        for app in applications {
+            print("\(app.localizedName ?? "Unspecified") \npid:\(app.processIdentifier) \n\(app.bundleURL?.absoluteString ?? "")")
+           
+            print("========================================")
+    
         }
     }
 
